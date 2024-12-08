6913491279c16faa906bdcbb80e7da5155d21a01
Michael Wood <mwood@data-crush.com>
2024-12-08 08:19:58 -0500

    add lib build option

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * as arcAlert from "./components/alert";
+export * as arcAlertDialog from "./components/alert-dialog";
+export * as arcMeter from "./components/meter";
+export * as arcTabGroup from "./components/tab-group";
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,31 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const plugins = [react()];
+
+  if (mode === 'lib') {
+    return {
+      plugins,
+      build: {
+        lib: {
+          entry: './src/index.ts',
+          name: 'AriaReadyComponents',
+          fileName: (format) => `aria-ready-components.${format}.js`,
+          formats: ['es', 'cjs'],
+        },
+        rollupOptions: {
+          external: ['react', 'react-dom'],
+          output: {
+            globals: {
+              react: 'React',
+              'react-dom': 'ReactDOM',
+            },
+          },
+        },
+      },
+    }
+  }
+
+  return { plugins }
 })
